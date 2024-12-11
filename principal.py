0a4ce04cf64681823b96b16d30e086dce65a1e38
af11090 <t013300420@unitru.edu.pe>
2024-12-11 14:37:59 -0500

    principal

--- principal.py
+++ principal.py
@@ -96,4 +96,5 @@ def predict_modelo3():
     return jsonify({'prediccion': resultado})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
